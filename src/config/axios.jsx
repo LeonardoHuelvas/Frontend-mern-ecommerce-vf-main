4cab9db2f86758fe1a69ab659560a50afe1dfb02
LeonardoHuelvas <lejhubo01@hotmail.com>
2023-03-05 04:00:01 -0500

    axios cliente

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Cliente axios
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
+    // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export default clienteAxios;
